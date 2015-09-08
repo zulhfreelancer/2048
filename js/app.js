128277b30243335ced8b56da6dc03b49422bba36
Zulhilmi Zainudin <zulhfreelancer@gmail.com>
2015-09-08 17:48:19 +0800

    Create keyboard service

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
-var app = angular.module('myApp', ['ngCookies', 'Game']);
+var app = angular.module('myApp', ['ngCookies', 'Game', 'Grid', 'Keyboard']);
 
-app.controller('myCtrl', ['$scope', 'GameManager', function ($scope, GameManager) {
-	this.game = GameManager;
+app.controller('myCtrl', ['$scope', 'GameManager', 'KeyboardService', function($scope, GameManager, KeyboardService) {
+
+    this.game = GameManager;
+
+    // Create a new game
+    this.newGame = function() {
+        KeyboardService.init();
+        this.game.newGame();
+        this.startGame();
+    };
+
+    this.startGame = function() {
+        var self = this;
+        KeyboardService.on(function(key) {
+            self.game.move(key);
+        });
+    };
+
+    // Create a new game on boot
+    this.newGame();
+
 }]);
